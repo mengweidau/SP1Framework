83436665caebf514e104307a97d473f97cd065a5
FrEakAZoiD-PC\FrEakAZoiD <shiroyami08gmail.com>
2016-08-26 10:19:49 +0800

    switches cpp and header

---
+++ SP1Framework/switches.cpp
@@ -0,0 +1,54 @@
+#include "switches.h"
+
+extern Console g_Console;
+extern SGameChar g_sChar;
+extern char maze[77][19];
+
+void switches()
+{
+	COORD c = g_Console.getConsoleSize();
+
+	bool on = false;
+
+	//when character stands on switch
+	if (currentMap == Map1)
+	{
+		if (maze[g_sChar.m_cLocation.X][g_sChar.m_cLocation.Y] == '/')
+		{
+			on = true;
+		}
+	}
+
+	if (currentMap == Map2)
+	{
+		if (maze[g_sChar.m_cLocation.X][g_sChar.m_cLocation.Y] == '/')
+		{
+			on = true;
+		}
+	}
+
+	if (currentMap == Map3)
+	{
+		if (maze[g_sChar.m_cLocation.X][g_sChar.m_cLocation.Y] == '/')
+		{
+			on = true;
+		}
+	}
+
+	if (on)
+	{
+		for (int y = 0; y < 19; y++)
+		{
+			c.Y = y;
+			for (int x = 0; x < 77; x++)
+			{
+
+				if (maze[x][y] == 'X')
+				{
+					maze[x][y] = ' ';
+				}
+				c.X = x;
+			}
+		}
+	}
+}
---
+++ SP1Framework/switches.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "game.h"
+#include "Framework\console.h"
+
+using namespace std;
+
+void switches();
